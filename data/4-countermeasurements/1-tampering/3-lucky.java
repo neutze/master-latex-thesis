770dcf0ea4c029795836c1a7fee13ea2b18c3f68
Johannes 'H1GHWAvE' Neutze <johannes.neutze@gmail.com>
2016-02-21 14:21:25 +0100

    content server

--- data/4-countermeasurements/1-tampering/3-lucky.java
+++ data/4-countermeasurements/1-tampering/3-lucky.java
@@ -8,48 +8,13 @@ public class Lucky Patcher {
 
     public static boolean checkInstall(final Context context) {
         boolean result = false;
-        String[] luckypatcher = new String[]{
-                // Lucky patcher
-                "com.dimonvideo.luckypatcher",
-                // Another lucky patcher
-                "com.chelpus.lackypatch",
-                // Black Mart alpha
-                "com.blackmartalpha",
-                // Black Mart
-                "org.blackmart.market",
-                // Lucky patcher 5.6.8
+        String luckypatcher =
+                // Lucky patcher 6.0.4
                 "com.android.vending.billing.InAppBillingService.LUCK",
-                // Freedom
-                "cc.madkite.freedom",
-                // All-in-one Downloader
-                "com.allinone.free",
-                // Get Apk Market
-                "com.repodroid.app",
-                // CreeHack
-                "org.creeplays.hack",
-                // Game Hacker
-                "com.baseappfull.fwd"
         };
 
-        for (String string : luckypatcher) {
-            if(checkInstallerName(context, string)){
-                result = true;
-            }
-
-            if (result) {
-                android.os.Process.killProcess(android.os.Process.myPid());
-            }
-        }
-
-        return result;
-    }
-
-    private static boolean checkInstallerName(Context context, String string) {
-        PackageInfo info;
-        boolean result = false;
-
         try {
-            info = context.getPackageManager().getPackageInfo(string, 0);
+            info = context.getPackageManager().getPackageInfo(luckypatcher, 0);
 
             if (info != null) {
                 android.os.Process.killProcess(android.os.Process.myPid());
@@ -62,6 +27,7 @@ public class Lucky Patcher {
         if (result) {
             android.os.Process.killProcess(android.os.Process.myPid());
         }
+        
         return result;
     }
 }
